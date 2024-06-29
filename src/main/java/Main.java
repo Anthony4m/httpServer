ccbcbc1f5b9a8b1576c419351a90176ab3489d59
Anthony4m <amoahanthony41@gmail.com>
2024-06-29 19:22:13 +0000

    pass stage

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,19 +13,48 @@ public class Main {
        serverSocket.setReuseAddress(true);
        clientSocket = serverSocket.accept(); // Wait for connection from client.
        clientSocket.getKeepAlive();
-       System.out.println("accepted new connection" + clientSocket.getInetAddress());
-       sendHttpResponse(clientSocket);
+       System.out.println("accepted new connection" + clientSocket);
+       handleHttpRequest(clientSocket);
        serverSocket.close();
      } catch (IOException e) {
        System.out.println("IOException: " + e.getMessage());
      }
   }
-    private static void sendHttpResponse(Socket clientSocket) throws IOException {
+    private static void handleHttpRequest(Socket clientSocket) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        String requestLine = reader.readLine(); // Read the request line
+
+        if (requestLine != null) {
+            System.out.println("Request Line: " + requestLine);
+
+            // Split the request line into parts
+            String[] parts = requestLine.split("\\s+");
+
+            if (parts.length >= 2) {
+                String method = parts[0];
+                String path = parts[1];
+                System.out.println("Method: " + method);
+                System.out.println("Path: " + path);
+
+                // Send HTTP/1.1 200 OK response
+                sendHttpResponse(clientSocket, path);
+            }
+        }
+    }
+
+    private static void sendHttpResponse(Socket clientSocket, String path) throws IOException {
         OutputStream outputStream = clientSocket.getOutputStream();
         PrintWriter writer = new PrintWriter(outputStream, true);
-
         // Prepare HTTP response
-        String httpResponse = "HTTP/1.1 200 OK\r\n\r\n";
+        String httpResponse;
+        switch (path){
+            case "/": httpResponse = "HTTP/1.1 200 OK\r\n\r\n";
+                break;
+            default:
+                httpResponse = "HTTP/1.1 404 Not Found\r\n\r\n";
+                break;
+        }
+
 
         // Send HTTP response
         writer.println(httpResponse);
